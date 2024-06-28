0abde9c87f25c0d89a6bbdc97d7cb8259c714623
Simon Rabuogi <symokevo@gmail.com>
2024-06-28 08:24:21 +0300

    Configure routes

--- config/routes.rb
+++ config/routes.rb
@@ -1,20 +1,10 @@
 Rails.application.routes.draw do
-  get 'notes/create'
-  get 'candles/create'
-  get 'heroes/index'
-  get 'heroes/show'
-  get 'heroes/new'
-  get 'heroes/create'
-  get 'heroes/edit'
-  get 'heroes/update'
-  get 'heroes/destroy'
   devise_for :users
-  # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
 
-  # Reveal health status on /up that returns 200 if the app boots with no exceptions, otherwise 500.
-  # Can be used by load balancers and uptime monitors to verify that the app is live.
-  get "up" => "rails/health#show", as: :rails_health_check
+  resources :heroes do
+    resources :candles, only: [:create]
+    resources :notes, only: [:create]
+  end
 
-  # Defines the root path route ("/")
-  # root "posts#index"
+  root 'heroes#index'
 end
