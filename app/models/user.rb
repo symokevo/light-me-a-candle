5c1a7934fca50eaecb691a87b5c2bd8c4e829af4
Simon Rabuogi <symokevo@gmail.com>
2024-07-07 10:29:32 +0300

    Authentication and Authorization
    - Add admin attribute to User model
    - Seed the database with an admin user
    - Update forms

--- app/models/user.rb
+++ app/models/user.rb
@@ -5,4 +5,10 @@ class User < ApplicationRecord
   has_many :heroes, foreign_key: 'created_by_id'
   has_many :candles
   has_many :notes
+
+  enum role: { user: 0, admin: 1 }
+  VALID_EMAIL_REGEX = /\A[\w+\-.]+@[a-z\d\-.]+\.[a-z]+\z/i
+  validates :email, presence: true, length: { maximum: 255 },
+  format: { with: VALID_EMAIL_REGEX },
+  uniqueness: true
 end
